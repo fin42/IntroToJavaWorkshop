10d9b29eedda06ac8a79eae5a696644ef65862cd
plooba <league@WTS9.attlocal.net>
2017-08-22 11:56:22 -0700

    Workshop Day 2
    By Finn

--- src/day1/robot/MyFirstJavaProgram.java
+++ src/day1/robot/MyFirstJavaProgram.java
@@ -1,15 +1,26 @@
 package day1.robot;
 
-import java.awt.Color;
 import org.jointheleague.graphical.robot.Robot;
 
 public class MyFirstJavaProgram {
 	public static void main(String[] args) {
-		
-		// START HERE
 
-	
-		
-		
+		// START HERE
+		System.out.println("Hello World");
+		Robot myRobot = new Robot("mini");
+		myRobot.setPenColor(255, 0, 0);
+		myRobot.penDown();
+		myRobot.turn(90);
+		myRobot.move(50);
+		myRobot.turn(90);
+		myRobot.setPenColor(0, 255, 0);
+		myRobot.move(50);
+		myRobot.turn(90);
+		myRobot.setPenColor(0, 0, 255);
+		myRobot.move(50);
+		myRobot.setPenColor(10, 78, 59);
+		myRobot.turn(90);
+		myRobot.move(50);
+
 	}
 }
--- src/day1/robot/TriangleShell.java
+++ src/day1/robot/TriangleShell.java
@@ -5,35 +5,46 @@ import org.jointheleague.graphical.robot.Robot;
 public class TriangleShell {
 
 	// 1. Create a new Robot
+	Robot myRobot = new Robot("mini");
 
-	
 	void go() {
-		drawTriangle(100); // 3. delete this line (used only for testing)
 
 		// 6. Make the robot go as fast as possible
 
 		// 4. make a variable to hold the length of the triangle and set it to 50
-
+		int length = 60;
 		// 7. Do the following (up to step 10) 60 times
-
+		for (int i = 0; i < 60; i++) {
 			// 9. Change the color of the pen to a random color
-	
-			// 8. Increase the length of the side by 10 pixels
-	
-			// 5. call your drawTriangle() method using your length variable
-	
-			// 10. Turn the tortoise 6 degrees to the right
 
+			// 8. Increase the length of the side by 10 pixels
+			length += 10;
+			// 5. call your drawTriangle() method using your length variable
+			drawTriangle(length);
+			// 10. Turn the tortoise 6 degrees to the right
+			myRobot.turn(6);
+		}
 	}
 
 	/* 2. fill in the method below to draw a triangle. Use the length variable for the size of the triangle. */
-	private void drawTriangle(int length) {
-		
-		
-		
+	private void drawTriangle(int length)
+
+	{
+
+		myRobot.setSpeed(20);
+		myRobot.turn(30);
+		myRobot.setRandomPenColor();
+		myRobot.penDown();
+		myRobot.move(length);
+		myRobot.turn(120);
+		myRobot.setRandomPenColor();
+		myRobot.move(length);
+		myRobot.turn(120);
+		myRobot.setRandomPenColor();
+		myRobot.move(length);
+
 	}
 
-	
 	public static void main(String[] args) {
 		new TriangleShell().go();
 	}
